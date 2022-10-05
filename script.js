45c0c261824b680826ce7f90a82b847fc32371f9
rahulch07 <87270395+rahulch07@users.noreply.github.com>
2022-10-06 00:14:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+var nav = document.getElementById('navlist');
+
+    function toggleNav () {       
+
+        if ( nav.style.display === "" )
+        nav.style.display = "block";
+
+        else
+        nav.style.display = "";
+    }
+
+
+    function windowResizeHandler () {
+        if ( screen.width > 500 )
+        nav.style.display = "";
+    }
+
+    window.addEventListener("resize", windowResizeHandler);
